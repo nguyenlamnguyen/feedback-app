12092d42e997ee43c5aa3ab76508a28101895b97
Nguyen Lam Nguyen <infernonguyen118@gmail.com>
2022-01-21 15:25:11 +0700

    Update

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,28 @@
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackItem({ item }) {
+    const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
+
+    return (
+        <Card reverse={true}>
+            <div className="num-display">{item.rating}</div>
+            <button onClick={() => deleteFeedback(item.id)} className="close">
+                <FaTimes color='purple' />
+            </button>
+            <button onClick={() => editFeedback(item)} className="edit">
+                <FaEdit color='purple'/>
+            </button>
+            <div className="text-display">{item.text}</div>
+        </Card>
+    )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
